51e2becf632609bf65ec8b372a191c795f19b0dd
Dominika <dominika_swieboda@outlook.com>
2025-11-19 09:56:17 +0100

    add basename in BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { PortfolioPage } from "./components/PortfolioPage.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/fluidweb">
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/portfolio/:id" element={<PortfolioPage />} />
